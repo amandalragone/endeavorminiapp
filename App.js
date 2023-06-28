85c065620786f87d84b7763925248e5b0f27622d
amandalragone <amandalragone@gmail.com>
2023-06-28 12:05:49 -0500

    First commit

---
+++ App.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View style={[styles.innerView]}>
+        <Text>Hello, World! This is an ERN mini app!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  innerView: {
+    flex: 1,
+    backgroundColor: '#0492C2',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
